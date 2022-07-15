a1394b8dd0f53ddbf81d353bd188a76f3494d530
Ted Chen <imaging8896@gmail.com>
2022-07-15 18:12:07 +0800

    [release] v0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.0.2 - 2022-07-15
+
+- No change. Just for testing uploading pypi
+
 ## 0.0.1 - 2022-07-15
 
 - First version
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name = "aws-easy-use",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Ted Chen",
     author_email = "imaging8896@gmail.com",
     description = "AWS boto3 wrapper. Make it easier and intuitive to use.",
